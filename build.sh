4cc91255ef0e243a61cdf5e0d0627198c2f77519
official-Ayush <ayushkumar.11pcm@gmail.com>
2026-08-03 19:56:23 +0530

    Auto-create superuser on build

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create the superuser using the environment variables
+python manage.py createsuperuser --noinput || true
